1942f88354d2261917deda495ca9241c7c2c7e41
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2018-10-27 13:03:59 -0400

    Added Heroku PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let apiRoutes = require("./routes/apiRoutes");		//Two different instances
 let htmlRoutes = require("./routes/htmlRoutes");	//of router are returned and stored 
 
 let app = express();
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
